9869b7ef3585639d1905869f20d075359e855efb
Aditya Shibu <as2397@hw.ac.uk>
2024-10-12 12:26:37 +0400

    Updated debrujin

--- lambda-Debrujin.py
+++ lambda-Debrujin.py
@@ -9,7 +9,7 @@ class LambdaTerm:
             term = self.term
 
         if isinstance(term, str):
-            # If the variable is found in the list of bound variables, return the De Bruijn index
+
             if term in bound_vars:
                 return str(len(bound_vars) - bound_vars.index(term))
             else:
@@ -17,12 +17,12 @@ class LambdaTerm:
         elif isinstance(term, tuple):
             operator = term[0]
             if operator == 'λ':
-                # Handle lambda abstraction
+
                 bound_var = term[1]
                 body = term[2]
                 return f"λ{self.de_bruijn(body, bound_vars + [bound_var])}"
             else:
-                # Handle function application
+
                 left = self.de_bruijn(term[0], bound_vars)
                 right = self.de_bruijn(term[1], bound_vars)
                 return f"({left}{right})"
@@ -30,7 +30,7 @@ class LambdaTerm:
     def __str__(self):
         return self.de_bruijn()
 
-# Test cases as nested tuples
+
 terms = [
     ('λ', 'x', ('λ', 'z', ('x', 'z'))),  # λxz.xz should output λλ21
     ('λ', 'x', ('λ', 'y', ('x', 'y'))),  # λxy.xy should output λλ21
@@ -39,4 +39,4 @@ terms = [
 
 for term in terms:
     l = LambdaTerm(term)
-    print(l)  # Converts and prints De Bruijn index representation
+    print(l)
